4b223441fd4a876256037343dc3ba4d0f9f2f34d
zMyLlama <noelgamsboel@gmail.com>
2022-11-22 11:32:57 +0100

    lambo answer

---
+++ pages/lambo.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import { useEffect } from "react";
+
+function Lamborghini() {
+    useEffect(() => {
+        location.href = "https://forms.gle/mpDeRWrcvU1xu1MaA"
+    }, [])
+
+    return ( 
+        <></>
+    );
+}
+
+
+export default Lamborghini;
